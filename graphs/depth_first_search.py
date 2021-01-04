168252d9ebb8d8c5df2b20a9345f6e167eac41a2
Matthew Poell <matthew.poell@gmail.com>
2021-01-04 14:40:37 -0800

    add graphs

---
+++ graphs/depth_first_search.py
@@ -0,0 +1,47 @@
+"""
+You're given a Node class that has a name 
+and an array of optional children nodes. 
+When put together, nodes form an acyclic
+tree-like structure.
+
+Implement the depth_first_search method
+on the Node class, which takes in an empty
+array, traverses the tree using DFS approach
+(navigating the tree from left to right), stores
+all of the nodes' names in the input array,
+and returns it. 
+"""
+
+
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        array.append(self.name)
+        for child in self.children:
+            child.depthFirstSearch(array)
+        return array
+
+
+test = {
+    "nodes": [
+        {"children": ["B", "C", "D"], "id": "A", "value": "A"},
+        {"children": ["E", "F"], "id": "B", "value": "B"},
+        {"children": [], "id": "C", "value": "C"},
+        {"children": ["G", "H"], "id": "D", "value": "D"},
+        {"children": [], "id": "E", "value": "E"},
+        {"children": ["I", "J"], "id": "F", "value": "F"},
+        {"children": ["K"], "id": "G", "value": "G"},
+        {"children": [], "id": "H", "value": "H"},
+        {"children": [], "id": "I", "value": "I"},
+        {"children": [], "id": "J", "value": "J"},
+        {"children": [], "id": "K", "value": "K"}
+    ],
+    "startNode": "A"
+}
